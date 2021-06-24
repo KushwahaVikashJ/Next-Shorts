4de159c9f2fd53ac14b8f4ef3ef8d25105da9e6a
Kushwaha Vikash <vikas.kushwaha@benchwork.in>
2021-06-24 18:15:04 +0530

    initial commit

--- components/meetups/MeetupDetails.js
+++ components/meetups/MeetupDetails.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Details(props) {
     return (
         <>
-           <Image src={props.image} alt={props.title} width='100%' height={500}/>
+           <Image src={props.image} alt={props.title} width={700} height={500}/>
            <h1>{props.title}</h1> 
            <address>{props.address}</address>
            <p>{props.description}</p>
